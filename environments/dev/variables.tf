f30568e7bff892d29253dd415d20d4705c1c8288
sigitp-git <sigit.priyanggoro@gmail.com>
2025-06-14 04:17:16 +0000

    Update EKS version from 1.30 to 1.32

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -25,7 +25,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.30"
+  default     = "1.32"
 }
 
 variable "environment" {
